2dbbd7d57b67ef63af7f26e3caaeb6da3a7ceb34
thetentmaker <obayyub@gmail.com>
2023-06-09 18:59:56 -0400

    transformer model

---
+++ src/model.py
@@ -0,0 +1,10 @@
+import transformer_lens
+import transformer_lens.utils as utils
+from transformer_lens.hook_points import (
+    HookedRootModule,
+    HookPoint,
+)  # Hooking utilities
+from transformer_lens import HookedTransformer, HookedTransformerConfig, FactoredMatrix, ActivationCache
+import numpy as np
+import torch as t
+
