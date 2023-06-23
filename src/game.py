ee243d59997da914e26ecc6865d6b6699f84f219
InfiniteSwerve <jackofthestrand@gmail.com>
2023-06-23 00:40:00 -0700

    added entropy calculations

--- src/game.py
+++ src/game.py
@@ -3,38 +3,49 @@ import math
 from typing import List, Tuple, Optional
 from enum import Enum
 from copy import deepcopy
+import numpy as np
+
 
 class State(Enum):
     DRAW = 0
     OVER = 1
     ONGOING = 2
 
-class Board():
+
+class Board:
     def __init__(self):
-        self.grid = [' '] * 9 # The game board
-        self.turn = 'X' #Who's turn is it?
-        self.game_state: State = State.ONGOING 
-        self.moves_played: list[int] = [] 
-        self.is_maximizer = True # Useful for the minimax algorithm
+        self.grid = [" "] * 9  # The game board
+        self.turn = "X"  # Who's turn is it?
+        self.game_state: State = State.ONGOING
+        self.moves_played: list[int] = []
+        self.is_maximizer = True  # Useful for the minimax algorithm
 
     # Internal
     def swap_turn(self) -> None:
         match self.turn:
-            case 'X':
-                self.turn = 'O'
+            case "X":
+                self.turn = "O"
                 self.is_maximizer = not self.is_maximizer
-            case 'O':
-                self.turn = 'X'
+            case "O":
+                self.turn = "X"
                 self.is_maximizer = not self.is_maximizer
             case _:
-                raise ValueError(f"Oh god how did we end up here, self.turn is {self.turn}")
+                raise ValueError(
+                    f"Oh god how did we end up here, self.turn is {self.turn}"
+                )
+
+    def is_over(self) -> bool:
+        if self.game_state != State.ONGOING:
+            return True
+        else:
+            return False
 
     # External
-    def get_possible_moves(self) -> List[int] :
+    def get_possible_moves(self) -> List[int]:
         if self.game_state != State.ONGOING:
             return []
         else:
-            return [i for i in range(9) if self.grid[i] == ' ']
+            return [i for i in range(9) if self.grid[i] == " "]
 
     # External
     def make_move(self, move: int):
@@ -48,14 +59,26 @@ class Board():
 
     # Internal
     def set_game_state(self) -> State:
-        win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8), 
-                          (0, 3, 6), (1, 4, 7), (2, 5, 8),
-                          (0, 4, 8), (2, 4, 6)]
+        win_conditions = [
+            (0, 1, 2),
+            (3, 4, 5),
+            (6, 7, 8),
+            (0, 3, 6),
+            (1, 4, 7),
+            (2, 5, 8),
+            (0, 4, 8),
+            (2, 4, 6),
+        ]
         for condition in win_conditions:
-            if self.grid[condition[0]] == self.grid[condition[1]] == self.grid[condition[2]] != ' ':
+            if (
+                self.grid[condition[0]]
+                == self.grid[condition[1]]
+                == self.grid[condition[2]]
+                != " "
+            ):
                 self.winner = self.grid[condition[0]]
                 return State.OVER
-        if ' ' not in self.grid:
+        if " " not in self.grid:
             return State.DRAW
         else:
             return State.ONGOING
@@ -67,11 +90,10 @@ class Board():
         else:
             raise ValueError("Game's not over yet or it's a draw!")
 
-
     # External
     def undo(self) -> None:
         last_move = self.moves_played.pop()
-        self.grid[last_move] = ' '
+        self.grid[last_move] = " "
         self.game_state = State.ONGOING
         self.swap_turn()
 
@@ -85,7 +107,10 @@ class Board():
         print(row2)
         print(row3)
 
-def generate_all_games(boards: List[Board], finished_boards: Optional[List[Board]] = None) -> List[Board]:
+
+def generate_all_games(
+    boards: List[Board], finished_boards: Optional[List[Board]] = None
+) -> List[Board]:
     if finished_boards == None:
         finished_boards = []
     ongoing_boards: list[Board] = []
@@ -97,25 +122,27 @@ def generate_all_games(boards: List[Board], finished_boards: Optional[List[Board
                 ongoing_boards.append(_board.make_move(move))
         else:
             finished_boards.append(board)
-    
+
     if ongoing_boards == []:
         return finished_boards
     else:
         return generate_all_games(ongoing_boards, finished_boards=finished_boards)
 
-def minimax(board: Board) -> int:
-    if (board.game_state == State.DRAW):
-        return 0
-    elif (board.game_state == State.OVER):
-        return 10 if board.turn == "O" else -10 
 
-    scores: list[int]= []
+def minimax(board: Board) -> int:
+    if board.game_state == State.DRAW:
+        return 0
+    elif board.game_state == State.OVER:
+        return 10 if board.turn == "O" else -10
+
+    scores: list[int] = []
     for move in board.get_possible_moves():
         board.make_move(move)
         scores.append(minimax(board))
         board.undo()
 
     return max(scores) if board.is_maximizer else min(scores)
+
 
 def get_best_moves(board: Board) -> List[int]:
     if board.is_maximizer:
@@ -135,10 +162,13 @@ def get_best_moves(board: Board) -> List[int]:
             bestScore = score
             bestMove.append((move, score))
     assert bestMove != []
-    bestMoves = [move for move, score in bestMove if score == bestScore ]
+    bestMoves = [move for move, score in bestMove if score == bestScore]
     return bestMoves
 
-def apply_best_moves(boards: List[Board], finished_boards: Optional[List[Board]] = None) -> List[Board]:
+
+def apply_best_moves(
+    boards: List[Board], finished_boards: Optional[List[Board]] = None
+) -> List[Board]:
     if finished_boards == None:
         finished_boards = []
     ongoing_boards = []
@@ -154,14 +184,15 @@ def apply_best_moves(boards: List[Board], finished_boards: Optional[List[Board]]
         return finished_boards
     else:
         return apply_best_moves(ongoing_boards, finished_boards)
-    
+
+
 def make_random_best_move(board: Board) -> None:
     if board.is_maximizer:
         bestScore = -math.inf
     else:
         bestScore = math.inf
 
-    moves: list[tuple[int,int]] = []
+    moves: list[tuple[int, int]] = []
     for move in board.get_possible_moves():
         board.make_move(move)
         score = minimax(board)
@@ -173,13 +204,33 @@ def make_random_best_move(board: Board) -> None:
             bestScore = score
             moves.append((move, score))
     assert moves is not []
-    bestMoves: list[int] = [move for move, score in moves if score == bestScore ]
+    bestMoves: list[int] = [move for move, score in moves if score == bestScore]
     bestMove = random.choice(bestMoves)
     board.make_move(bestMove)
 
-# def play_game():
-#     board = Board()
-#     while board.game_state == State.ONGOING:
-#         make_random_best_move(board)
-#     return board
+def entropy(board: Board) -> float:
+    moves = board.get_possible_moves()
+    return -(1 / len(moves)) * np.log2(1 / len(moves))
 
+
+# goes over number of unique subsequences of tic tac toe
+def tree_walk(board: Board) -> Tuple[float, int]:
+    if board.is_over():
+        return (0, 10 - len(board.moves_played))
+    total_entropy = entropy(board)
+    seen = 1
+    for move in board.get_possible_moves():
+        board.make_move(move)
+        extra_entropy, extra_seen = tree_walk(board)
+        total_entropy += extra_entropy
+        seen += extra_seen
+        board.undo()
+    return (total_entropy, seen)
+
+
+"""
+coin flips
+1. p(H) * log2(p(H)) + p(T) * log2(p(T))
+2. p(H | H) * log2(p(H | H)) + p(T | H) * log2(p(T | H)) + p(H | T) * log2(p(H | T)) + p(T | T) * log2(p(T | T)) 
+
+"""
